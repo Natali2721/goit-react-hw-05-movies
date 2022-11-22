6cd870e61a97281bc34ded57445f148eaa6e6fdd
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-11-22 15:21:59 +0200

    Update Home.styled.js

--- src/Pages/Home.styled.js
+++ src/Pages/Home.styled.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 export const MovieLink = styled(Link)`
   text-decoration: none;
+  padding: 2px 8px;
 
   &:hover {
     border-radius: 5px;
@@ -11,6 +12,7 @@ export const MovieLink = styled(Link)`
 `;
 export const MovieList = styled.ul`
   padding: 20px 0 20px;
+  margin: 0;
   display: flex;
   flex-direction: column;
   row-gap: 10px;
@@ -18,4 +20,5 @@ export const MovieList = styled.ul`
 
 export const Title = styled.h2`
   margin-top: 40px;
+  margin-bottom: 0;
 `;

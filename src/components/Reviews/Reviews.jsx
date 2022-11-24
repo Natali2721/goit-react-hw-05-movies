6f7d2c222f71da1c34f8865b3c3fd5ffd7e7564d
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-11-24 11:19:11 +0200

    Update Reviews.jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,6 +1,6 @@
-import { getReviews } from 'Api/apiService';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { getReviews } from 'Api/apiService';
 
 const Reviews = () => {
   const [reviewsList, setReviewsList] = useState([]);

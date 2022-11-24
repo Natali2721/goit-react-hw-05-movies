3368373a90914c15b46fd8ee99f14ca61ac112a1
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-11-24 11:26:30 +0200

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -11,23 +11,25 @@ const Cast = () => {
   }, [movieId]);
   return (
     <CastList>
-      {castList.map(({ id, name, profile_path, character }) => (
-        <CastItem key={id}>
-          <img
-            src={
-              profile_path
-                ? `https://image.tmdb.org/t/p/w200${profile_path}`
-                : `https://thumbs.dreamstime.com/z/no-image-available-icon-photo-camera-flat-vector-illustration-132483141.jpg`
-            }
-            alt="actor"
-            loading="lazy"
-            width={120}
-            height={180}
-          />
-          <Name>{name}</Name>
-          <Character> Character: {character}</Character>
-        </CastItem>
-      ))}
+      {castList.length > 0
+        ? castList.map(({ id, name, profile_path, character }) => (
+            <CastItem key={id}>
+              <img
+                src={
+                  profile_path
+                    ? `https://image.tmdb.org/t/p/w200${profile_path}`
+                    : `http://www.suryalaya.org/images/no_image.jpg`
+                }
+                alt="actor"
+                loading="lazy"
+                width={120}
+                height={180}
+              />
+              <Name>{name}</Name>
+              <Character> Character: {character}</Character>
+            </CastItem>
+          ))
+        : "Sorry, there isn't any info :("}
     </CastList>
   );
 };

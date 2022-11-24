83ba50bca272e6eaef06ddcb3f3962d66884f693
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-11-24 11:19:45 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,18 @@
 import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Cast from 'components/Cast/Cast';
-import Reviews from 'components/Reviews/Reviews';
-import Home from 'Pages/Home/Home';
-import MovieDetails from 'Pages/MovieDetails/MovieDetails';
-import Movies from 'Pages/Movies/Movies';
+import { SharedLayout } from 'components/SharedLayout/SharedLayout';
 
-import { Container, Header, Link } from './App.styled';
+const Cast = lazy(() => import('components/Cast/Cast'));
+const Reviews = lazy(() => import('components/Reviews/Reviews'));
+const Home = lazy(() => import('Pages/Home/Home'));
+const MovieDetails = lazy(() => import('Pages/MovieDetails/MovieDetails'));
+const Movies = lazy(() => import('Pages/Movies/Movies'));
 
 export const App = () => {
   return (
-    <Container>
-      <Header>
-        <nav>
-          <Link to="/" end>
-            Home
-          </Link>
-          <Link to="/movies">Movies</Link>
-        </nav>
-      </Header>
-      <Routes>
-        <Route path="/" element={<Home />} />
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
         <Route path="/movies" element={<Movies />} />
 
         <Route path="movies/:movieId" element={<MovieDetails />}>
@@ -28,7 +20,7 @@ export const App = () => {
           <Route path="reviews" element={<Reviews />} />
         </Route>
         <Route path="*" element={<Home />} />
-      </Routes>
-    </Container>
+      </Route>
+    </Routes>
   );
 };
